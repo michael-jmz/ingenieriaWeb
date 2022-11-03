42e209a89db01a9b7be7febeb8a141c621be2b8f
michael-jmz <www.michaelj@hotmail.es>
2022-11-03 15:58:26 -0500

    maquetando con grids & flex

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Happy Pets</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+	<link rel="icon" href="images/logo.png" sizes="32x32" />
+    <link rel="icon" href="images/logo.png" sizes="192x192" />
+    <link rel="apple-touch-icon" href="images/logo.png" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
+</head>
+<body>
+	<div class="contenedor">
+	<header class="cabeceraPrincipal">
+		<section class="logotipo"><img src="images/logotipoPet.png"></section>
+		<nav class="menuPrincipal">
+			<a href="#">Inicio</a>
+			<a href="internas/servicios.php">Servicios</a>
+			<a href="#">Productos</a>
+			<a href="#">Adopción</a>
+			<a href="#">Contactos</a>
+		</nav>
+	</header>
+	<section class="sliderPrincipal">
+		<img src="images/slider1.jpg" title="slider">
+	</section>
+
+	<section class="titulo"><h2 class="colorAzul mayusculas">Adopción</h2></section>
+
+	<section class="adopcion1">
+		<img src="images/dog1.jpg" title="firulais" descrition="Perro de adopcion">
+		<article class ="card1">
+			<h3 class="mayusculas colorAzul">Firulais</h3>
+			<p class="derecha">
+			Lorem ipsum dolor sit amet, consectetur 
+			adipiscing elit, sed do eiusmod tempor 
+			incididunt ut labore et dolore magna aliqua. 
+			Sed egestas egestas fringilla phasellus faucibus
+			scelerisque. Et molestie ac feugiat sed lectus 
+			vestibulum mattis ullamcorper velit. Nisl vel
+			pretium lectus quam id leo in vitae turpis. 
+			Laoreet non curabitur gravida arcu ac tortor 
+			dignissim.
+			</p>
+			
+			<div class="buttonDererecha">
+				<button class="button button1">Mas Informacion</button>
+			</div>
+		</article>
+	</section>
+
+	<section class="adopcion2">
+		<img src="images/dog2.jpg" title="dog1">
+		<article class="card1">
+			<h3 class="mayusculas colorAzul">Firulais</h3>
+			<p class="">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+				sed do eiusmod tempor incididunt ut labore et dolore 
+				magna aliqua. Sed egestas egestas fringilla 
+				phasellus faucibus scelerisque. 
+				Et molestie ac feugiat sed lectus vestibulum 
+				mattis ullamcorper velit. Nisl vel pretium lectus 
+				quam id leo in vitae turpis. Laoreet non curabitur 
+				gravida arcu ac tortor dignissim.</p>
+			<div class="buttonDererecha">
+				<button class="button button1">Mas Informacion</button>
+			</div>
+		</article>
+	</section>
+	<section class="doctor">
+		<h3>Doctores</h3>
+	</section>
+	<section class="sponsor">
+		<h3 class="colorAzul">Sponsor</h3>
+	</section>
+	
+<!--FOOTER-->
+	<footer class="piePagina">
+		<section class="derechos">
+			<h6>Derechos Reservados UTPL 2022</h6>
+		</section>
+		<nav class="redesSociales">
+			<a href=""><i class="fa-brands fa-facebook"></i></a>
+			<a href=""><i class="fa-brands fa-instagram"></i></a>
+			<a href=""><i class="fa-brands fa-tiktok"></i></a>
+		</nav>
+	</footer>
+	</div>
+
+</body>
+</html>
